a63d4a7b65e8403c357d3d3fbbf52aa50018e7b9
selluri-deloitte <100287436+selluri-deloitte@users.noreply.github.com>
2022-02-24 17:03:00 +0530

    Update steps.bat

--- steps.bat
+++ steps.bat
@@ -1,10 +1,10 @@
 @echo off
-title running installation commands
-echo installing robotframework
-pip install robotframework
-echo installing robotframework-seleniumlibrary
-pip install  robotframework-seleniumlibrary
-echo setting path variable for chrome driver
-setx path %cd%
-set path %cd%
+::  title running installation commands
+:: echo installing robotframework
+:: pip install robotframework
+:: echo installing robotframework-seleniumlibrary
+:: pip install  robotframework-seleniumlibrary
+:: echo setting path variable for chrome driver
+:: setx path %cd%
+:: set path %cd%
 pause
